8e9c6551cde459f9be04e1b9994f26624b32c03e
jzhnice <1749597552@qq.com>
2021-04-20 11:15:04 +0800

    Initial commit

---
+++ src/edu/xcdq/ceshi.java
@@ -0,0 +1,18 @@
+package edu.xcdq;
+
+/**
+ * @version 1.0
+ * @author: jiazhihao
+ * @date: 2021-04-13 15:06
+ */
+public class ceshi {
+    public static void main(String[] args) {
+
+
+    Main main = new Main();
+    main.sayhello();
+    main.duijue();
+    main.jxm();
+}
+
+}
